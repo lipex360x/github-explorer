72d7ccffea3e66bd2b16b3b4d9d124f9923c9677
Felipe Borges <lipex360@gmail.com>
2021-01-03 22:40:55 -0300

    Axios connected

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,64 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
+
 import * as S from './styles'
 
 import logo from '../../assets/logo.svg'
 
+interface RepositoryProps {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard = () => {
+  const [repositories, setRepositories] = useState<RepositoryProps[]>([])
+  const [newRepo, setNewRepo] = useState('')
+
+  async function handleAddRepository (event:FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault()
+    const response = await api.get<RepositoryProps>(`repos/${newRepo}`)
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+    setNewRepo('')
+  }
+
   return (
     <>
       <img src={logo} alt="" srcSet=""/>
       <S.Title>Explorer on GitHub</S.Title>
-      <S.Form>
-        <input placeholder="Insert the repository name" />
+      <S.Form onSubmit={handleAddRepository}>
+
+        <input
+          placeholder="Insert the repository name"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
+
         <button>Search</button>
       </S.Form>
 
       <S.Repositories>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/32343054?s=460&u=307a9b437754f3fe41e8dbb3872cd3282f68a387&v=4"
-            alt="Felipe"
-          />
-          <div>
-            <strong>Julius</strong>
-            <p>Personal Finance Platform</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="#">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/32343054?s=460&u=307a9b437754f3fe41e8dbb3872cd3282f68a387&v=4"
-            alt="Felipe"
-          />
-          <div>
-            <strong>Julius</strong>
-            <p>Personal Finance Platform</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/32343054?s=460&u=307a9b437754f3fe41e8dbb3872cd3282f68a387&v=4"
-            alt="Felipe"
-          />
-          <div>
-            <strong>Julius</strong>
-            <p>Personal Finance Platform</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </S.Repositories>
 
     </>
